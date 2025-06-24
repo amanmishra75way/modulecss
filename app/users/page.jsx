7fb89d96638c48a1a61f744d195bd7c64706eba2
amanmishra75way <amishra.75way@gmail.com>
2025-06-24 18:41:41 +0530

    Refactor global and module CSS usage in page component

--- app/users/page.jsx
+++ app/users/page.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import style from "./index.module.css";
 const page = () => {
-  return <div className={style.container}>This is box</div>;
+  return (
+    <>
+      <div className={style.container}>This is module css</div>
+      <div className="container">This is global css</div>
+    </>
+  );
 };
 
 export default page;
